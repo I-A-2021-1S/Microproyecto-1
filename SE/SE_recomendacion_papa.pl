ed7f66176f6acc497514c17b7e6739c45ac051c8
Juan David Pino Ramirez <jpinor@unal.edu.co>
2021-03-21 17:34:33 -0500

    SE con mensajes

--- SE/SE_recomendacion_papa.pl
+++ SE/SE_recomendacion_papa.pl
@@ -15,7 +15,7 @@ sugerir(X) :- write('Ingresa la altitud del terreno (msnm): '), read(AL),
     verificar_agua(AG2,AG),
     verificar_luz(L2,L),
     verificar_ph(PH2,PH),
-    write('El tipo de papa recomendado según las condiciones ingresadas es: '), 	
+    /* write(''), */ 	
     recomendar(X,AL,R,C,AG2,L2,PH2).
 
 /*Reglas de verificacion agua*/
@@ -34,20 +34,19 @@ verificar_ph(basico,PH):-PH>7.5.
 verificar_ph(neutro,PH):-PH=<7.5, PH>=6.5.
 
 
+recomendar(mucha_agua,_,_,_,AG2,_,_):-AG2=alta,write('Con tanta agua el cultivo morirá o no será muy sostenible debido a el alto desperdicio de agua, intenta gastar menos en pro de la sostenibilidad y el exito del cultivo.'),!.
+recomendar(poca_agua,_,_,_,AG2,_,_):-AG2=baja,write('Con poca agua el cultivo morirá pues seguramente se secará, intenta con valores más optimos.'),!.
+recomendar(poca_luz,_,_,_,_,L2,_):-L2=baja,write('La poca luz no favorece el cecimiento de la planta, considera aumentar este número.'),!.
+recomendar(mucha_luz,_,_,_,_,L2,_):-L2=alta,write('Tanta luz puede ser contraproducente para el desarrollo de la planta, intenta con valores más bajos.'),!.
+recomendar(ph_acido,_,_,_,_,_,PH2):-PH2=acido,write('En un tierra con PH bajo el cultivo de papa no prospera, busca valores más neutros en la escala.'),!.
+recomendar(ph_basico,_,_,_,_,_,PH2):-PH2=basico,write('En un tierra con PH alto el cultivo de papa no prospera, busca valores más neutros en la escala.'),!.
 
-recomendar(mucha_agua,_,_,_,AG,_,_):-AG=alta,!.
-recomendar(poca_agua,_,_,_,AG,_,_):-AG=baja,!.
-recomendar(poca_luz,_,_,_,_,L,_):-L=baja,!.
-recomendar(mucha_luz,_,_,_,_,L,_):-L=alta,!.
-recomendar(ph_acido,_,_,_,_,_,PH):-PH=acido,!.
-recomendar(ph_basico,_,_,_,_,_,PH):-PH=basico,!.
-
-recomendar(pastusa_suprema,A,R,C,_,_,_):-(A>=2000,A=<3500),(R>=15,R=<20),(C>=12,C=<18).
-recomendar(diacol_capiro,A,R,C,_,_,_):-(A>=2300,A=<3200), (R>=25,R=<30),(C>=14,C=<17).
-recomendar(parda_pastua,A,R,C,_,_,_):-(A>=2000,A=<3500), (R>=28,R=<33),(C>=10,C=<17).
-recomendar(tocarreña,A,R,C,_,_,_):-(A>=2600,A=<3200), (R>=18,R=<23),(C>=15,C=<20).
-recomendar(criolla,A,R,C,_,_,_):-(A>=1800,A=<3200), (R>=7,R=<12),(C>=10,C=<20).
-recomendar(ica_unica,A,R,C,_,_,_):-(A>=2600,A=<3200), (R>=30,R=<45),(C>=13,C=<24).
-recomendar(perla_negra,A,R,C,_,_,_):-(A>=2500,A=<2700), (R>=27,R=<37),(C>=10,C=<17).
-recomendar(rubi,A,R,C,_,_,_):-(A>=2750,A=<3200), (R>=40,R=<47),(C>=10,C=<23).
-recomendar(ica_purace,A,R,C,_,_,_):-(A>=2200,A=<3000), (R>=30,R=<37),(C>=11,C=<18).
+recomendar(pastusa_suprema,A,R,C,_,_,_):-(A>=2000,A=<3500),(R>=15,R=<20),(C>=12,C=<18),write('El tipo de papa recomendado según las condiciones ingresadas es: ').
+recomendar(diacol_capiro,A,R,C,_,_,_):-(A>=2300,A=<3200), (R>=25,R=<30),(C>=14,C=<17),write('El tipo de papa recomendado según las condiciones ingresadas es: ').
+recomendar(parda_pastua,A,R,C,_,_,_):-(A>=2000,A=<3500), (R>=28,R=<33),(C>=10,C=<17),write('El tipo de papa recomendado según las condiciones ingresadas es: ').
+recomendar(tocarreña,A,R,C,_,_,_):-(A>=2600,A=<3200), (R>=18,R=<23),(C>=15,C=<20),write('El tipo de papa recomendado según las condiciones ingresadas es: ').
+recomendar(criolla,A,R,C,_,_,_):-(A>=1800,A=<3200), (R>=7,R=<12),(C>=10,C=<20),write('El tipo de papa recomendado según las condiciones ingresadas es: ').
+recomendar(ica_unica,A,R,C,_,_,_):-(A>=2600,A=<3200), (R>=30,R=<45),(C>=13,C=<24),write('El tipo de papa recomendado según las condiciones ingresadas es: ').
+recomendar(perla_negra,A,R,C,_,_,_):-(A>=2500,A=<2700), (R>=27,R=<37),(C>=10,C=<17),write('El tipo de papa recomendado según las condiciones ingresadas es: ').
+recomendar(rubi,A,R,C,_,_,_):-(A>=2750,A=<3200), (R>=40,R=<47),(C>=10,C=<23),write('El tipo de papa recomendado según las condiciones ingresadas es: ').
+recomendar(ica_purace,A,R,C,_,_,_):-(A>=2200,A=<3000), (R>=30,R=<37),(C>=11,C=<18),write('El tipo de papa recomendado según las condiciones ingresadas es: ').
