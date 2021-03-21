d433a13f01d6b353f72b9c43a20f409699624c4d
Juan David Pino Ramirez <jpinor@unal.edu.co>
2021-03-21 18:28:35 -0500

    SE terminado

--- SE/SE_recomendacion_papa.pl
+++ SE/SE_recomendacion_papa.pl
@@ -33,13 +33,20 @@ verificar_ph(acido,PH):-PH<6.5.
 verificar_ph(basico,PH):-PH>7.5.
 verificar_ph(neutro,PH):-PH=<7.5, PH>=6.5.
 
+recomendar(poca_altitud,AL,_,_,_,_,_):- AL<1800,write('Los suelos a baja altura no son aptos para el cultivo de papa, busca un terreno más alto. '),!.
+recomendar(mucha_altitud,AL,_,_,_,_,_):- AL>3500,write('Los suelos muy altos no son aptos para el cultivo de papa, busca un terreno más bajo. '),!.
+
+recomendar(baja_temperatura,_,_,C,_,_,_):- C<10,write('A muy bajas temperaturas los cultivos de papas no son óptimos, los cultivos se pueden perder o la cosecha no es buena. '),!.
+recomendar(alta_temperatura,_,_,C,_,_,_):- C>24,write('A muy altas temperaturas no se puede cultivar papa, pues la papa es propia de los climas templados. '),!.
+
+recomendar(poca_luz,_,_,_,_,L2,_):-L2=baja,write('La poca luz no favorece el cecimiento de la planta, considera aumentar este número.'),!.
+recomendar(mucha_luz,_,_,_,_,L2,_):-L2=alta,write('Tanta luz puede ser contraproducente para el desarrollo de la planta, intenta con valores más bajos.'),!.
+
+recomendar(ph_acido,_,_,_,_,_,PH2):-PH2=acido,write('En un tierra con PH bajo el cultivo de papa no prospera, busca valores más neutros en la escala.'),!.
+recomendar(ph_basico,_,_,_,_,_,PH2):-PH2=basico,write('En un tierra con PH alto el cultivo de papa no prospera, busca valores más neutros en la escala.'),!.
 
 recomendar(mucha_agua,_,_,_,AG2,_,_):-AG2=alta,write('Con tanta agua el cultivo morirá o no será muy sostenible debido a el alto desperdicio de agua, intenta gastar menos en pro de la sostenibilidad y el exito del cultivo.'),!.
 recomendar(poca_agua,_,_,_,AG2,_,_):-AG2=baja,write('Con poca agua el cultivo morirá pues seguramente se secará, intenta con valores más optimos.'),!.
-recomendar(poca_luz,_,_,_,_,L2,_):-L2=baja,write('La poca luz no favorece el cecimiento de la planta, considera aumentar este número.'),!.
-recomendar(mucha_luz,_,_,_,_,L2,_):-L2=alta,write('Tanta luz puede ser contraproducente para el desarrollo de la planta, intenta con valores más bajos.'),!.
-recomendar(ph_acido,_,_,_,_,_,PH2):-PH2=acido,write('En un tierra con PH bajo el cultivo de papa no prospera, busca valores más neutros en la escala.'),!.
-recomendar(ph_basico,_,_,_,_,_,PH2):-PH2=basico,write('En un tierra con PH alto el cultivo de papa no prospera, busca valores más neutros en la escala.'),!.
 
 recomendar(pastusa_suprema,A,R,C,_,_,_):-(A>=2000,A=<3500),(R>=15,R=<20),(C>=12,C=<18),write('El tipo de papa recomendado según las condiciones ingresadas es: ').
 recomendar(diacol_capiro,A,R,C,_,_,_):-(A>=2300,A=<3200), (R>=25,R=<30),(C>=14,C=<17),write('El tipo de papa recomendado según las condiciones ingresadas es: ').
